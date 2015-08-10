440e625ef57c5ebd1faf95b9da5990d98a0f63f8
Keyan Ghazi-Zahedi <keyan.zahedi@googlemail.com>
2015-08-10 23:27:42 +0200

    Working on bug fixes

--- src/MC.jl
+++ src/MC.jl
@@ -2,6 +2,8 @@ module MC
 
 using Shannon
 
+ϵ = 0.0000001
+
 # data[w', w, a] -> D_KL(p(w'|w,a)||p(w'|a)) = MI(W';W|A)
 function MC_W(data::Matrix{Int64}; base=2)
   @assert (size(data)[2] == 2) "MC_W required two columns (W,A)."
@@ -9,7 +11,7 @@ function MC_W(data::Matrix{Int64}; base=2)
   w1      = data[1:end-1,1]
   a1      = data[1:end-1,2]
   m       = maximum(data[:,1])
-  pw2w1a1 = fe3p(hcat(w2,w1,a1))
+  pw2w1a1 = Shannon.fe3p(hcat(w2,w1,a1))
   return CMI(pw2w1a1, base=base) / log(base,m)
 end
 
@@ -20,7 +22,7 @@ function MC_A(data::Matrix{Int64}; base=2)
   w1      = data[1:end-1,1]
   a1      = data[1:end-1,2]
   m       = maximum(data[:,1])
-  pw2a1w1 = fe3p(hcat(w2,a1,w1))
+  pw2a1w1 = Shannon.fe3p(hcat(w2,a1,w1))
   return 1.0 - (CMI(pw2a1w1, base=base) / log(base,m))
 end
 
@@ -33,18 +35,19 @@ function C_A(data::Matrix{Int64}; base=2)
   s1      = data[1:end-1,1]
   a1      = data[1:end-1,2]
   m       = maximum(data[:,1])
-  ps2s1a1 = fe3p(hcat(s2,s1,a1))
+  ps2s1a1 = Shannon.fe3p(hcat(s2,s1,a1))
 
   ps1a1   = sum(ps2s1a1, 1)
   pa1     = sum(ps2s1a1, (1,2))
+  ps1     = sum(ps2s1a1, (1,3))
 
   # p(s'|s,a) = p(s',s,a) / p(s,a)
   ps2_c_s1a1 = zeros(size(ps2s1a1))
   for s2_index = 1:s_max
     for s1_index = 1:s_max
       for a1_index = 1:a_max
-        if abs(ps1a1[s1_index, a1_index]) > ϵ
-          ps2_c_s1a1[s2_index, s1_index, a1_index] = ps2s1a1[s2_index, s1_index, a1_index] / ps1a1[s1_index, a1_index]
+        if abs(ps1a1[1, s1_index, a1_index]) > ϵ
+          ps2_c_s1a1[s2_index, s1_index, a1_index] = ps2s1a1[s2_index, s1_index, a1_index] / ps1a1[1, s1_index, a1_index]
         end
       end
     end
@@ -55,7 +58,7 @@ function C_A(data::Matrix{Int64}; base=2)
   for s2_index = 1:s_max
     for a1_index = 1:a_max
       for s1_index = 1:s_max
-        ps2_do_a1[s2_index, a1_index] = ps2_do_a1[s2_index, a1_index] + ps2_c_s1a1[s2_index, s1_index, a1_index] * ps1(s1_index)
+        ps2_do_a1[s2_index, a1_index] = ps2_do_a1[s2_index, a1_index] + ps2_c_s1a1[s2_index, s1_index, a1_index] * ps1[1,s1_index, 1]
       end
     end
   end
@@ -65,7 +68,7 @@ function C_A(data::Matrix{Int64}; base=2)
   for a1_index = 1:a_max
     for s1_index = 1:s_max
       if abs(ps1[s1_index]) > ϵ
-        pa1_c_s1[a1_index, s1_index] = ps1a1[s1_index, a1_index] / ps1[s1_index]
+        pa1_c_s1[a1_index, s1_index] = ps1a1[1, s1_index, a1_index] / ps1[1, s1_index, 1]
       end
     end
   end
@@ -86,13 +89,13 @@ function C_A(data::Matrix{Int64}; base=2)
   for s2_index = 1:s_max
     for s1_index = 1:s_max
       for a1_index = 1:a_max
-        if abs(ps1a1[s1_index, a1_index]) > ϵ && abs(ps2_do_a1[s2_index, a1_index]) > ϵ && abs(ps2_do_s1[s2_index, s1_index]) > ϵ
-          r = r + ps1a1[s1_index, a1_index] * ps2_do_a1[s2_index, a1_index] * (log(base, ps2_do_a1[s2_index, a1_index]) - log(base, ps2_do_s1[s2_index, s1_index]))
+        if abs(ps1a1[1, s1_index, a1_index]) > ϵ && abs(ps2_do_a1[s2_index, a1_index]) > ϵ && abs(ps2_do_s1[s2_index, s1_index]) > ϵ
+          r = r + ps1a1[1, s1_index, a1_index] * ps2_do_a1[s2_index, a1_index] * (log(base, ps2_do_a1[s2_index, a1_index]) - log(base, ps2_do_s1[s2_index, s1_index]))
         end
       end
     end
   end
-  return r
+  return r / log(base, s_max)
 end
 
 # data[s', s, a] -> D_KL(p(s'|s,a)||p(s'|a))
@@ -104,15 +107,16 @@ function C_W(data::Matrix{Int64}; base=2)
   s1      = data[1:end-1,1]
   a1      = data[1:end-1,2]
   m       = maximum(data[:,1])
-  ps2s1a1 = fe3p(hcat(s2,s1,a1))
+  ps2s1a1 = Shannon.fe3p(hcat(s2,s1,a1))
 
   ps1     = sum(ps2s1a1, (1,3))
   pa1     = sum(ps2s1a1, (1,2))
   ps2s1   = sum(ps2s1a1, 3)
   ps1a1   = sum(ps2s1a1, 1)
+  ps2a1   = sum(ps2s1a1, 2)
 
   # p(s'|s) = p(s',s) / p(s)
-  ps2_c_s1 = zeors(s_max, s_max)
+  ps2_c_s1 = zeros(s_max, s_max)
   for s2_index = 1:s_max
     for s1_index = 1:s_max
       if abs(ps1[s1_index]) > ϵ
@@ -126,7 +130,7 @@ function C_W(data::Matrix{Int64}; base=2)
   for s2_index = 1:s_max
     for a1_index = 1:a_max
       if abs(pa1[a1_index]) > ϵ
-        ps2_c_a1[s2_index, a1_index] = ps2a1[s2_index, a1_index] / pa1[a1_index]
+        ps2_c_a1[s2_index, a1_index] = ps2a1[s2_index, 1, a1_index] / pa1[a1_index]
       end
     end
   end
@@ -135,8 +139,8 @@ function C_W(data::Matrix{Int64}; base=2)
   pa1_c_s1 = zeros(a_max, s_max)
   for a1_index = 1:a_max
     for s1_index = 1:s_max
-      if abs(ps1[s1_index]) > ϵ
-        pa1_c_s1[a1_index, s1_index] = ps1a1[s1_index, a1_index] / ps1[s1_index]
+      if abs(ps1[1, s1_index, 1]) > ϵ
+        pa1_c_s1[a1_index, s1_index] = ps1a1[1, s1_index, a1_index] / ps1[1, s1_index, 1]
       end
     end
   end
@@ -154,8 +158,9 @@ function C_W(data::Matrix{Int64}; base=2)
   r = 0
   for s1_index = 1:s_max
     for s2_index = 1:s_max
-      if abs(ps2s1[s2_index, s1_index]) > ϵ && abs(ps2_c_s1[s2_index, s1_index]) > ϵ && abs(phat_s2_c_s1[s2_index, s1_index]) > ϵ
-        r = r + ps2s1[s2_index, s1_index] * (log(base, ps2_c_s1[s2_index, s1_index]) - log(base, phat_s2_c_s1[s2_index, s1_index]))
+      if abs(ps2s1[s2_index, s1_index, 1]) > ϵ && abs(ps2_c_s1[s2_index, s1_index]) > ϵ && abs(phat_s2_c_s1[s2_index, s1_index]) > ϵ
+        r = r + ps2s1[s2_index, s1_index, 1] * (log(base, ps2_c_s1[s2_index, s1_index]) - log(base, phat_s2_c_s1[s2_index, s1_index]))
+      end
     end
   end
 
@@ -163,14 +168,14 @@ function C_W(data::Matrix{Int64}; base=2)
 end
 
 # Morphological Computation
-function MC(data::Matrix{Int64}; base=2, mode="MC A", pseudocount=0)
+function quantify(data::Matrix{Int64}; base=2, mode="MC A", pseudocount=0)
   modes      = ["MC A",
-                "MC W",
-                "ASOC A",
-                "ASOC W",
-                "C A",
-                "C W"
-                ]
+  "MC W",
+  "ASOC A",
+  "ASOC W",
+  "C A",
+  "C W"
+  ]
   umodes     = map(x->uppercase(x), modes)
   known_mode = uppercase(mode) in umodes
   pmodes     = map(x->", $x",modes)
